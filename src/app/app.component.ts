5272827d9ab27a1330a2b800b5b2155b5e3ac875
Junior <vilmarmendesjunior@hotmail.com>
2020-03-27 20:01:07 -0300

    feat: mock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { PathService } from './services/path/path.service';
 import { AlgorithmService } from './services/algorithm/algorithm.service';
 import { Component, OnInit } from '@angular/core';
+import pathsMock from './mock/paths.mock';
 
 @Component({
   selector: 'app-root',
@@ -11,17 +12,7 @@ export class AppComponent implements OnInit {
   constructor(private algorithmService: AlgorithmService, private pathService: PathService) {}
 
   ngOnInit() {
-    const paths = [
-      { originCity: 'Vancouver', destinationCity: 'Ottawa', distance: 50 },
-      { originCity: 'Vancouver', destinationCity: 'Los Angeles', distance: 200 },
-
-      { originCity: 'Los Angeles', destinationCity: 'Ottawa', distance: 300 },
-      { originCity: 'Los Angeles', destinationCity: 'Vancouver', distance: 200 },
-
-      { originCity: 'Ottawa', destinationCity: 'Vancouver', distance: 50 },
-      { originCity: 'Ottawa', destinationCity: 'Los Angeles', distance: 300 }
-    ];
-    this.pathService.setPaths(paths);
+    this.pathService.setPaths(pathsMock);
     this.algorithmService.start();
   }
 }
---
+++ src/app/mock/paths.mock.ts
@@ -0,0 +1,15 @@
+export default [
+  { originCity: 'Vancouver', destinationCity: 'Los Angeles', distance: 50 },
+  { originCity: 'Vancouver', destinationCity: 'Ottawa', distance: 20 },
+  { originCity: 'Vancouver', destinationCity: 'Florida', distance: 10 },
+  { originCity: 'Vancouver', destinationCity: 'London', distance: 500 },
+
+  { originCity: 'Los Angeles', destinationCity: 'Ottawa', distance: 30 },
+  { originCity: 'Los Angeles', destinationCity: 'Florida', distance: 40 },
+  { originCity: 'Los Angeles', destinationCity: 'London', distance: 700 },
+
+  { originCity: 'Ottawa', destinationCity: 'Florida', distance: 1300 },
+  { originCity: 'Ottawa', destinationCity: 'London', distance: 20 },
+
+  { originCity: 'Florida', destinationCity: 'London', distance: 650 }
+];
