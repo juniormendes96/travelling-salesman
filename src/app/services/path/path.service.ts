2ac35fcf67073ee9fa79a199e352ce1b267968e2
Junior <vilmarmendesjunior@hotmail.com>
2020-03-27 18:21:54 -0300

    feat: services and path functions

---
+++ src/app/services/path/path.service.ts
@@ -0,0 +1,39 @@
+import { Path } from './../../models/path.model';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PathService {
+  private paths: Path[];
+
+  constructor() {}
+
+  getPaths(): Path[] {
+    return this.paths;
+  }
+
+  setPaths([...paths]): void {
+    this.paths = paths;
+  }
+
+  getDistanceBetween(cityA: string, cityB: string): number {
+    const path = this.findPath(cityA, cityB);
+    if (!path) {
+      throw new Error('Path not found.');
+    }
+
+    return path.distance;
+  }
+
+  private findPath(cityA: string, cityB: string): Path {
+    const path = this.paths.find(path => {
+      return (
+        (path.originCity === cityA && path.destinationCity === cityB) ||
+        (path.originCity === cityB && path.destinationCity === cityA)
+      );
+    });
+
+    return path;
+  }
+}
