a416c925d90ab0a8d8219fdecdfc91cc684e923f
Junior <vilmarmendesjunior@hotmail.com>
2020-03-27 18:35:59 -0300

    test: path service

--- src/app/services/path/path.service.spec.ts
+++ src/app/services/path/path.service.spec.ts
@@ -3,10 +3,36 @@ import { TestBed } from '@angular/core/testing';
 import { PathService } from './path.service';
 
 describe('PathService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: PathService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [PathService]
+    });
+
+    service = TestBed.get(PathService);
+    service.setPaths([
+      { originCity: 'Vancouver', destinationCity: 'Ottawa', distance: 300 },
+      { originCity: 'Florida', destinationCity: 'Los Angeles', distance: 500 },
+      { originCity: 'London', destinationCity: 'Manchester', distance: 200 }
+    ]);
+  });
 
   it('should be created', () => {
-    const service: PathService = TestBed.get(PathService);
     expect(service).toBeTruthy();
+  });
+
+  it('should return the paths', () => {
+    expect(service.getPaths().length).toBe(3);
+  });
+
+  it('should get the distance between two cities', () => {
+    expect(service.getDistanceBetween('Vancouver', 'Ottawa')).toBe(300);
+    expect(service.getDistanceBetween('Ottawa', 'Vancouver')).toBe(300);
+  });
+
+  it('should throw an error when the city is not on the list', () => {
+    expect(() => service.getDistanceBetween('Any city', 'Ottawa')).toThrowError();
+    expect(() => service.getDistanceBetween('Ottawa', 'Any city')).toThrowError();
   });
 });
